e614ca684425f7526288a979fbe7fd879aa7483b
lui07804 <luisa.buderer@ise.fraunhofer.de>
2021-06-08 11:39:51 +0200

    New Port 8083

--- spring-boot-security-jwt-auth-mongodb-master/src/main/java/com/bezkoder/spring/jwt/mongodb/controllers/AuthController.java
+++ spring-boot-security-jwt-auth-mongodb-master/src/main/java/com/bezkoder/spring/jwt/mongodb/controllers/AuthController.java
@@ -51,7 +51,7 @@ public class AuthController {
 	@Autowired
 	JwtUtils jwtUtils;
 
-	@PostMapping("/signin")
+	@PostMapping("signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
 		Authentication authentication = authenticationManager.authenticate(
